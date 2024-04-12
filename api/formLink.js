5789aea332fba01f6fbde6fe861e614a16be865d
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-04-12 02:59:43 -0500

    Updated html form.

--- api/formLink.js
+++ api/formLink.js
@@ -1,7 +1,17 @@
-export async function GET() { 
+export function GET() {
     return new Response(/*html*/`
-    <form action="/api/addLink">
-        <input type="text" placeholder="Article link" name="link">
-        <input type="submit">
-    </form>`, { headers: new Headers({ "Content-Type": "text/html" }) });
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Article link form</title>
+        </head>
+        <body>
+            <form action="/api/addLink">
+                <input type="text" placeholder="Article link" name="link">
+                <input type="submit">
+            </form>
+        </body>
+    </html>`, { headers: new Headers({ "Content-Type": "text/html" }) });
 }
